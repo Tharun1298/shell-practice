c9121740beac95d1a0b2c52fa741e1d3efdbbc18
THARUN1998 <adamatharun99@gmail.com>
2025-09-25 08:25:25 +0530

    session 6

---
+++ 05-condition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+NUMBER=$1
+
+if[$NUMBER -lt 10 ]; then
+    echo "given number $NUMBER is less than 10"
+else 
+    echo "given number $NUMBER is gretaer than or equal to 10"
+fi
+
+
+# -gt
+# -eq
+# -ne
