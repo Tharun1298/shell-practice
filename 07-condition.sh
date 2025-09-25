385fb2359a455aa1e5f4453d82f704c1981e5099
THARUN1998 <adamatharun99@gmail.com>
2025-09-25 08:44:30 +0530

    session 6

--- 07-condition.sh
+++ 07-condition.sh
@@ -4,8 +4,8 @@ echo "please enter the number"
 read NUMBER
 
 if [ $((NUMBER % 2)) -eq 0 ]; then
-    echo "given number $NUMBER is even'
+    echo "given number $NUMBER is even"
 else 
-    echo " given number $NUMBER  is odd"
+    echo "given number $NUMBER  is odd"
 
 fi
