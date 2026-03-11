bd2564e05eafd316a590e98ee46c923fc3ad70ec
Adama Tharun <adamatharun99@gmail.com>
2026-03-11 18:27:18 +0530

    THARUN

---
+++ 04-Variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1: Hey $PERSON2, Long time to see you in Alaska"
+echo "$PERSON2: Hi $PERSON1, Yes finally not on zoom"
+echo "$PERSON1: How is everything going there?"
+echo "$PERSON2: Everywhere is your story of epsteil $PERSON1"
+echo "$PERSON1: Ignore that, we started war23"
+
