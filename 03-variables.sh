292672abd1c035cb1fcd661fbe8cb7dc3fffaa31
Adama Tharun <adamatharun99@gmail.com>
2026-03-11 16:47:28 +0530

    Tharun

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=john
+PERSON2=Ram
+
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hi $PERSON1"
+echo "$PERSON1: Waht are you doing now"
+echo "$PERSON2: I am studying deviops"
