67865c2f1c266410beb4fd9ebd88e255679cced0
Adama Tharun <adamatharun99@gmail.com>
2026-03-12 15:50:19 +0530

    Tharuns pratcice

--- 12-installation.sh
+++ 12-installation.sh
@@ -6,6 +6,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
 
     echo "ERROR: Please run with user who are having root privileges"
+    exit 1
 
 fi
 
@@ -14,7 +15,7 @@ dnf install mysql -y
 if [ $? eq 0 ]; then
 
     echo "Installation is successfull"
-
+    exit 1
 else 
 
     echo "Installation is unsusccesfull"
