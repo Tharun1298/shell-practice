8344afdc8345249cdbc94a5bc664112a8361717b
Adama Tharun <adamatharun99@gmail.com>
2026-03-12 16:48:46 +0530

    Tharuns pratcice

--- 15-colors.sh
+++ 15-colors.sh
@@ -9,29 +9,20 @@ N="\e[0m"
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-
     echo -e "$R run with sudo user $N"
     exit 1
-
 fi
 
 VALIDATE(){
-
     if [ $1 -eq 0 ]; then
-
         echo  -e " installation of $2 is $G successfull $N"
-
     else
-
         echo -e "Installation of $2 is $R unsuccssfull $N"
-
     fi
-
 }
 
 
 dnf list insatalled mysql
-
 if [ $? -ne 0 ]; then
     dnf install mysql -y
     VALIDATE $? "MySQL"
