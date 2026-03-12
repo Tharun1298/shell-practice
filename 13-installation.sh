c6a11f0a3b3774ebd6529c8fc9cebaa3039b14b2
Adama Tharun <adamatharun99@gmail.com>
2026-03-12 15:58:15 +0530

    Tharuns pratcice

---
+++ 13-installation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]; then
+
+    echo "run with sudo user"
+
+    exit 1
+
+fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]; then
+
+    echo " installation of MySQL is successfull"
+
+else
+
+    echo "Installation of MySQL is unsuccssfull;
+
+fi
+
